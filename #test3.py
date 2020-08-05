2d711b34b7a38c2beb5a5dca74f5d6b69ef4461c
Maria Gusarova <67602938+Maria1205@users.noreply.github.com>
2020-08-05 13:20:30 -0400

    Update #test3.py

--- #test3.py
+++ #test3.py
@@ -9,17 +9,21 @@ def calculate_the_number_plate():
 		amount_of_switches = customer_ID//999
 
 		right_letter = chr(97+amount_of_switches//26**2)
+		#calculates the right letter, same as 'abcdefghijklmnopqrstuvwxyz'[amount_of_switches//26**2]
 
 		middle_letter = chr(97+amount_of_switches%26**2//26)
+		#calculates the middle letter, same as 'abcdefghijklmnopqrstuvwxyz'[amount_of_switches%26**2//26]
 
 		left_letter = chr(97+amount_of_switches%26**2%26)
+		#calculates the middle letter, same as 'abcdefghijklmnopqrstuvwxyz'[amount_of_switches%26**2%26]
 	
 		result = left_letter+middle_letter+right_letter+str(series_number).rjust(3,'0')
 
 		return pd.DataFrame({'ID':[customer_ID],'NP':[result]})
+		#returns the result as a data frame, containing the customer ID and the number plate itself
 	
 	except ValueError:
-		
+		#if the inut number is negative or contains non-digit characters or is bigger than 17558423 (the maximum value), a Value Error is returned
 		return 'Invalid Input'
 
 # some test cases:
