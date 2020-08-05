ecd0d70850e109af7736a29b020f635ef3a780a5
Maria Gusarova <67602938+Maria1205@users.noreply.github.com>
2020-08-05 15:31:34 -0400

    Update #test3.py

--- #test3.py
+++ #test3.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 def calculate_the_number_plate():
 	try:
-		customer_ID = int(input('Type in the customer ID number. It should be a whole number between 0 and 17558423 : '))
+		vehicle_ID = int(input('Type in the vehicle ID number. It should be a whole number between 0 and 17558423 : '))
 
-		series_number=customer_ID%999+1
+		series_number=vehicle_ID%999+1
 
-		amount_of_switches = customer_ID//999
+		amount_of_switches = vehicle_ID//999
 
 		right_letter = chr(97+amount_of_switches//26**2)
 		#calculates the right letter, same as 'abcdefghijklmnopqrstuvwxyz'[amount_of_switches//26**2]
@@ -19,7 +19,7 @@ def calculate_the_number_plate():
 	
 		result = left_letter+middle_letter+right_letter+str(series_number).rjust(3,'0')
 
-		return pd.DataFrame({'ID':[customer_ID],'NP':[result]})
+		return pd.DataFrame({'ID':[vehicle_ID],'NP':[result]})
 		#returns the result as a data frame, containing the customer ID and the number plate itself
 	
 	except ValueError:
